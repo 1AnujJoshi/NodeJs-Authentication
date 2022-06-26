160e52e9e2b64c81bc669bcf0d9f12ca385b8cde
Anuj Joshi <anujjoshi.87210@gmail.com>
2022-06-26 15:41:39 +0530

    added some changes to config

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
   }
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${port}`);
 });
